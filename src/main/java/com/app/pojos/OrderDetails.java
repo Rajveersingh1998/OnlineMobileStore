6d795cf35d2f0c9ed0cfd0e8cdcb527fff1b1daf
Rajveer_singh_46806 <rajveersinghbhamra9@gmail.com>
2022-03-26 22:11:16 +0530

    updated

--- src/main/java/com/app/pojos/OrderDetails.java
+++ src/main/java/com/app/pojos/OrderDetails.java
@@ -1,10 +1,18 @@
 package com.app.pojos;
 
+import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
 
 //@Entity
 //@Table(name="orderdetails")
 public class OrderDetails extends BaseEntity{
 
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate orderDate;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate deliveryDate;
+	
+	
 	
 }
